246aa79af9aecd31fb3b99fd51a26be52af97ee9
ayushjai2002 <aj905490@gmail.com>
2024-06-11 18:47:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from "./components/navbar";
+import Image from "./Image/wallpaper.webp";
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	return (
+		<React.Fragment>
+			<Navbar/>
+      <div class="background-image">
+        <div class="text-overlay">
+            <h1>Journey With Confidence migrate with us</h1>
+            <p>This is a description text over the background image.</p>
+        </div>
     </div>
-  );
+      
+		</React.Fragment>
+	);
 }
 
 export default App;
